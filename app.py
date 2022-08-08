c7500c833ab43535b517abe2de58b4324c7ad866
Kevin Sebastian <Kevin_Sebastian@intuit.com>
2022-08-07 19:32:08 -0700

    random colors

--- app.py
+++ app.py
@@ -6,28 +6,29 @@ from dash.dependencies import Input, Output, State
 import pandas as pd
 import plotly as py
 import plotly.graph_objs as go
-
+from random import randrange
 
 ###### Define your variables #####
 tabtitle = 'Titanic!'
-color1='#92A5E8'
-color2='#8E44AD'
-color3='#FFC300'
+color_list = ['paleturquoise', 'lavenderblush', 'aliceblue', 'burlywood', 'deeppink', 'lightyellow', 'beige', 'greenyellow', 'darkorange']
+color1 = '#92A5E8'
+color2 = '#8E44AD'
+color3 = '#FFC300'
 sourceurl = 'https://www.kaggle.com/c/titanic'
-githublink = 'https://github.com/plotly-dash-apps/304-titanic-dropdown'
-
+githublink = 'https://github.com/ksebastian/304-titanic-dropdown'
 
 ###### Import a dataframe #######
-df = pd.read_csv("https://raw.githubusercontent.com/austinlasseter/plotly_dash_tutorial/master/00%20resources/titanic.csv")
-df['Female']=df['Sex'].map({'male':0, 'female':1})
-df['Cabin Class'] = df['Pclass'].map({1:'first', 2: 'second', 3:'third'})
-variables_list=['Survived', 'Female', 'Fare', 'Age']
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/austinlasseter/plotly_dash_tutorial/master/00%20resources/titanic.csv")
+df['Female'] = df['Sex'].map({'male': 0, 'female': 1})
+df['Cabin Class'] = df['Pclass'].map({1: 'first', 2: 'second', 3: 'third'})
+variables_list = ['Survived', 'Female', 'Fare', 'Age']
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title=tabtitle
+app.title = tabtitle
 
 ####### Layout of the app ########
 app.layout = html.Div([
@@ -49,32 +50,33 @@ app.layout = html.Div([
 @app.callback(Output('display-value', 'figure'),
               [Input('dropdown', 'value')])
 def display_value(continuous_var):
-    grouped_mean=df.groupby(['Cabin Class', 'Embarked'])[continuous_var].mean()
-    results=pd.DataFrame(grouped_mean)
+    grouped_mean = df.groupby(['Cabin Class', 'Embarked'])[continuous_var].mean()
+    results = pd.DataFrame(grouped_mean)
+
     # Create a grouped bar chart
     mydata1 = go.Bar(
         x=results.loc['first'].index,
         y=results.loc['first'][continuous_var],
         name='First Class',
-        marker=dict(color=color1)
+        marker=dict(color=color_list[randrange(9)])
     )
     mydata2 = go.Bar(
         x=results.loc['second'].index,
         y=results.loc['second'][continuous_var],
         name='Second Class',
-        marker=dict(color=color2)
+        marker=dict(color=color_list[randrange(9)])
     )
     mydata3 = go.Bar(
         x=results.loc['third'].index,
         y=results.loc['third'][continuous_var],
         name='Third Class',
-        marker=dict(color=color3)
+        marker=dict(color=color_list[randrange(9)])
     )
 
     mylayout = go.Layout(
         title='Grouped bar chart',
-        xaxis = dict(title = 'Port of Embarkation'), # x-axis label
-        yaxis = dict(title = str(continuous_var)), # y-axis label
+        xaxis=dict(title='Port of Embarkation'),  # x-axis label
+        yaxis=dict(title=str(continuous_var)),  # y-axis label
 
     )
     fig = go.Figure(data=[mydata1, mydata2, mydata3], layout=mylayout)
